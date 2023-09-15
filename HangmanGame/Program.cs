c12d1e2cb5ed1502757305c5de861b5c9ffa003a
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-09-15 14:57:03 +0100

    Add project files.

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "hangman";
+            bool gameLost = false;
+            int lives = 6;
+
+
+            string[] characters = word.Select(x => new string(x, 1)).ToArray();
+            string[] guesses = characters;
+
+
+            for (int i = 0 ; i < guesses.Length; i++)
+            {
+                guesses[i] = "_";
+            }
+            //char[] characters = word.ToCharArray();
+
+            characters = word.Select(x => new string(x, 1)).ToArray();
+
+
+            while (gameLost == false)
+            {
+                bool letterGuessed = false;
+
+                for (int i = 0; i < guesses.Length; i++)
+                {
+                    Console.Write(guesses[i]);
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Please input a letter: ");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+
+                //char charInput = char.Parse(input);
+
+                for (int i =  0; i < characters.Length; i++)
+                {
+                    if (input == characters[i])
+                    {
+                        guesses[i] = characters[i];
+
+
+                        if (String.Join("", guesses) == word)
+                        {
+                            Console.WriteLine(word);
+                            Console.WriteLine("You win!!");
+                            Console.ReadLine();
+                            Environment.Exit(0);
+                        }
+                        letterGuessed = true;
+                    }
+                }
+
+                if (letterGuessed == false)
+                {
+                    lives = lives - 1;
+                    Console.WriteLine("Lives remaining: {0}", lives);
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("You lose!!");
+                        gameLost = true;
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StringManipulation/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.PortableExecutable;
+
+namespace StringManipulation
+{
+    public class positionOfCharacter
+    {
+        public string? nameOfWord { get; set; }
+        public string? c { get; set; }
+        public int pos { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            string contents = "";
+            List<positionOfCharacter> list = new List<positionOfCharacter>();
+            try
+            {
+                // Get file name.
+                string path = @"Files/Words.txt";
+                // Get path name.
+                string filename = Path.GetFileName(path);
+                // Open the text file using a stream reader. Read into a string
+                using (var sr = new StreamReader(path))
+                {
+                    // Read the stream as a string, and write the string to the console.
+                    contents = sr.ReadToEnd();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("---------");
+            //Store each word into an array using split on '\n'
+            var array = contents.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            string letterToSearch = "o";
+
+            //Iterate through array printing out each word.
+            foreach (var word in array)
+            {
+                //Console.WriteLine($"{word} : number of chars: {word.Length}  ");
+                //Console.WriteLine($"occurence of letterToSearch: {word.Count(f => f == 'e')}");
+                //Console.WriteLine($"Element at 0: {word.ElementAt(0)}");
+                //Console.WriteLine($"index of letterToSearch : {word.IndexOf('e')}");
+                //Console.WriteLine("---------");
+
+                string letter = letterToSearch.ToLower();
+
+                string wordToSearch = word.ToLower();
+                int index = 0;
+                if (wordToSearch.Contains(letter))
+                {
+                    // Find index where letter is
+                    if (wordToSearch.IndexOf(letter) != -1)
+                    {
+                        while ((index = wordToSearch.IndexOf(letter, index)) != -1)
+                        {
+                            Console.WriteLine("word: " + wordToSearch + "  " + letter + " found at position " + "  " + index);
+                            index++;
+                            list.Add(new positionOfCharacter() { nameOfWord = wordToSearch, c = letter, pos = index });
+
+                        }
+                    }
+                }
+                Console.WriteLine("");
+
+
+            }
+            foreach (var li in list)
+            {
+                // Console.WriteLine($"word: {li.nameOfWord}  letter: {li.c}   position: {li.pos}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

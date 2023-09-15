fa84ae54a523da77ac0373b9a5437151bea54ed9
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-09-15 15:50:19 +0100

    Added text file (full support not implemented)

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,38 @@ namespace HangmanGame
     {
         static void Main(string[] args)
         {
-            string word = "hangman";
+            string contents = "";
+            try
+            {
+                string path = @"Files/Words.txt";
+
+                string filename = Path.GetFileName(path);
+
+                using (var sr = new StreamReader(path))
+                {
+                    contents = sr.ReadToEnd();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(contents);
+            string[] array = contents.Split('\n', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            //Bug with spliting string, will fix at later date
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Random rnd = new Random();
+            int number = rnd.Next(1, array.Length);
+
+            string word = "hangman";//array[number];
             bool gameLost = false;
             int lives = 6;
 
@@ -24,10 +57,10 @@ namespace HangmanGame
             {
                 guesses[i] = "_";
             }
-            //char[] characters = word.ToCharArray();
 
+
+            
             characters = word.Select(x => new string(x, 1)).ToArray();
-
 
             while (gameLost == false)
             {
@@ -39,7 +72,7 @@ namespace HangmanGame
                 }
                 Console.WriteLine();
 
-                Console.WriteLine("Please input a letter: ");
+                Console.Write("Please input a letter: ");
                 string input = Console.ReadLine();
                 input = input.ToLower();
 
@@ -66,6 +99,11 @@ namespace HangmanGame
                 if (letterGuessed == false)
                 {
                     lives = lives - 1;
+
+
+
+
+
                     Console.WriteLine("Lives remaining: {0}", lives);
                     if (lives == 0)
                     {
